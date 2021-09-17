692e14835083b66c7f747da60c653f67895f2c12
vradchenko <v.radchenko@cian.ru>
2021-09-17 23:05:23 +0300

    bootstrap added

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,24 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Test
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={'container'}>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="url" aria-label="Search" disabled={true}/>
+                        <button className="btn btn-outline-dark" type="submit">share</button>
+                    </form>
+                </div>
+            </nav>
+            <div className={'row align-content-center'}>
+                <button className={'btn btn-success btn-lg'}>+</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
