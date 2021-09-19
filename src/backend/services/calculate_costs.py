eeb2209c3f79c2c6575131ae813b9e462de7c439
vradchenko <v.radchenko@cian.ru>
2021-09-19 18:30:00 +0300

    part of calculate logic

--- src/backend/services/calculate_costs.py
+++ src/backend/services/calculate_costs.py
@@ -1,5 +1,6 @@
 import asyncio
 from dataclasses import dataclass
+from typing import Optional
 
 from backend.entityies.calcuate_costs import Bill, BillType, Calculation, Group, Person
 
@@ -8,6 +9,13 @@ from backend.entityies.calcuate_costs import Bill, BillType, Calculation, Group,
 class GroupBalance:
     group: Group
     balance: int
+
+
+@dataclass
+class PaymentAction:
+    payer: str
+    recepient: str
+    amount: int
 
 
 async def calculate_spendinds(calc: Calculation) -> None:
@@ -24,9 +32,9 @@ async def calculate_spendinds(calc: Calculation) -> None:
         general_total_spent += group.general_spent
         alcohol_total_spent += group.alcohol_spent
         meat_total_spent += group.meat_spent
-    meat_per_person = round(meat_total_spent / meat_eaters, 2)
-    alcohol_per_person = round(alcohol_total_spent / alcohol_drinkers, 2)
-    general_per_person = round(general_total_spent / general_persons, 2)
+    meat_per_person = meat_total_spent / meat_eaters
+    alcohol_per_person = alcohol_total_spent / alcohol_drinkers
+    general_per_person = general_total_spent / general_persons
     payers = []
     for group in calc.groups:
         group_balance = GroupBalance(
@@ -36,9 +44,48 @@ async def calculate_spendinds(calc: Calculation) -> None:
         group_balance.balance += meat_per_person * group.meat_eaters_count
         group_balance.balance += alcohol_per_person * group.alcohol_drinkers_count
         group_balance.balance += general_per_person * group.persons_count
+        group_balance.balance = group_balance.balance
         payers.append(group_balance)
 
-    print(payers)
+    result_actions: list[PaymentAction] = []
+    sub_total = sum([i.balance for i in payers])
+    while any([payer.balance for payer in payers]):
+        payer = get_next_proficit(payers)
+        recepient = get_next_deficit(payers)
+        if payer.balance > abs(recepient.balance):
+            result_actions.append(
+                PaymentAction(
+                    payer=payer.group.name,
+                    recepient=recepient.group.name,
+                    amount=abs(recepient.balance),
+                )
+            )
+            payer.balance -= abs(recepient.balance)
+            recepient.balance = 0
+        else:
+            result_actions.append(
+                PaymentAction(
+                    payer=payer.group.name,
+                    recepient=recepient.group.name,
+                    amount=payer.balance,
+                )
+            )
+            recepient.balance += payer.balance
+            payer.balance = 0
+
+
+def get_next_proficit(actors: list[GroupBalance]) -> Optional[GroupBalance]:
+    for i in actors:
+        if i.balance > 0:
+            return i
+    return None
+
+
+def get_next_deficit(actors: list[GroupBalance]) -> Optional[GroupBalance]:
+    for i in actors:
+        if i.balance < 0:
+            return i
+    return None
 
 
 config = Calculation(
@@ -69,7 +116,7 @@ config = Calculation(
             ]
         ),
         Group(
-            name='Family 3',
+            name='Family 2',
             persons=[
                 Person(
                     name='Dima',
@@ -92,7 +139,82 @@ config = Calculation(
                     bill_type=BillType.meat,
                 ),
             ]
-        )
+        ),
+        Group(
+            name='Family 3',
+            persons=[
+                Person(
+                    name='Dima',
+                    drinks_alcohol=True,
+                    eat_meat=True,
+                ),
+                Person(
+                    name='Vika',
+                    drinks_alcohol=True,
+                    eat_meat=True,
+                ),
+            ],
+            spending=[
+                Bill(
+                    amount=300,
+                    bill_type=BillType.general,
+                ),
+                Bill(
+                    amount=1390,
+                    bill_type=BillType.meat,
+                ),
+                Bill(
+                    amount=15000,
+                    bill_type=BillType.general,
+                )
+            ]
+        ),
+        Group(
+            name='Family 4',
+            persons=[
+                Person(
+                    name='Dima',
+                    drinks_alcohol=True,
+                    eat_meat=True,
+                ),
+                Person(
+                    name='Vika',
+                    drinks_alcohol=True,
+                    eat_meat=True,
+                ),
+            ],
+            spending=[
+                Bill(
+                    amount=300,
+                    bill_type=BillType.general,
+                ),
+                Bill(
+                    amount=1390,
+                    bill_type=BillType.meat,
+                ),
+                Bill(
+                    amount=3678,
+                    bill_type=BillType.alcohol,
+                )
+            ]
+        ),
+        Group(
+            name='Family 5',
+            persons=[
+                Person(
+                    name='Dima',
+                    drinks_alcohol=False,
+                    eat_meat=True,
+                )
+            ],
+            spending=[
+                Bill(
+                    amount=300,
+                    bill_type=BillType.general,
+                )
+            ]
+        ),
+
     ]
 )
 
